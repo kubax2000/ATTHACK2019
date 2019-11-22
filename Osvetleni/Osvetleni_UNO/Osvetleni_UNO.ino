55be7b147d825e56c438938b441ffbbdf121c15e
Avik0 <mejlforbros@gmail.com>
2019-11-22 13:49:14 +0100

    Test zapojeni osvetleni

--- Osvetleni/Osvetleni_UNO/Osvetleni_UNO.ino
+++ Osvetleni/Osvetleni_UNO/Osvetleni_UNO.ino
@@ -1,11 +1,27 @@
-void setup() {
-    byte led_sever = 3;
-    pinMode(3, OUTPUT);
+const int sever = 3;
+const int jih = 5;
+const int vychod = 6;
+const int zapad = 9;
+const int krizovatka = 10;
+int jas;
+
+
+void setup() {    
+    pinMode(sever, OUTPUT);
+    pinMode(jih, OUTPUT);
+    pinMode(vychod, OUTPUT);
+    pinMode(zapad, OUTPUT);
+    Serial.begin(9600);
 }
 
 void loop() {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
-    digitalWrite(LED_BUILTIN, LOW); 
-    delay(1000);  
+    jas += 10;
+    if(jas >= 240) jas = 0;
+    analogWrite(sever, jas);
+    analogWrite(jih, jas);
+    analogWrite(vychod, jas);
+    analogWrite(zapad, jas);
+    analogWrite(krizovatka, jas);
+    delay(100);
+    Serial.println(jas);
 }
