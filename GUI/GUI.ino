b06504e3b294a4737eff086024427b5b06c005dd
Andrii <andrijlutchenko@gmail.com>
2019-11-22 22:26:05 +0100

    RFID - Done

--- GUI/GUI.ino
+++ GUI/GUI.ino
@@ -7,8 +7,8 @@
 #define ROWS 4
 #define COLS 4
 
-#define SDA_PIN 36
-#define RST_PIN 28
+#define SDA_PIN 9
+#define RST_PIN 8
 
 char keys[ROWS][COLS] = {
 {'1','2','3','A'},
@@ -16,16 +16,27 @@ char keys[ROWS][COLS] = {
 {'7','8','9','C'},
 {'*','0','#','D'}
 };
+
 byte colPins[COLS] = {5,4,3,2};
 byte rowPins[ROWS] = {9,8,7,6};
- 
+bool login = false; 
 MFRC522 rfid(SDA_PIN, RST_PIN);
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3,POSITIVE);
 Keypad keypad = Keypad(makeKeymap(keys),rowPins,colPins,ROWS,COLS);
 
+void def(){
+  char Str1[]= "Welcome Indetify"; 
+  char Str2[]= "yourself with ID";
+   
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print(Str1);
+    lcd.setCursor(0,1);
+    lcd.print(Str2);
+    delay(150);
+}
 
-
- 
+   
 void setup()
 {
   Wire.begin();
@@ -35,60 +46,58 @@ void setup()
   
   Serial.println("Power ON");
   lcd.begin(16,2);
-
-  
 }
 
- 
-void Pressed () {
-  
-  }
 
 void loop()
-{
-/*
- lcd.clear();
- lcd.setCursor(0,0); 
- char Str[] = {"Welcome, indetify yourself with ID"};
+  {
+ 
+  if(!login){
 
- lcd.print(Str);
- int size = sizeof(Str);
- for(int i = 0;i < size ;i++){
-   delay(500);
-   lcd.scrollDisplayLeft();
- }*/
+    def();
+    Serial.println("Login start");
+    if ( ! rfid.PICC_IsNewCardPresent()) {
+      return;
+    }
+    
+    if ( ! rfid.PICC_ReadCardSerial()) {
+      return;
+    }
+    String id= "";
+    Serial.print("Card: "); 
+    for( byte i = 0; i < rfid.uid.size; i++){
+    Serial.print(rfid.uid.uidByte[i] < 0x10 ? "0" : " ");
+    Serial.print(rfid.uid.uidByte[i], HEX);
+    id.concat(String(rfid.uid.uidByte[i] < 0x10 ? "0" : " "));
+    id.concat(String(rfid.uid.uidByte[i], HEX));
+  } 
   
-
-   if ( ! rfid.PICC_IsNewCardPresent()) {
-   return;
+    Serial.println();
+    Serial.print("Info: ");
+    id.toUpperCase();
+    if (id.substring(1) == "59 5D 94 A2")
+    {
+      lcd.clear();
+      lcd.setCursor(0,0); 
+      lcd.print("Authorizet access");
+      Serial.println("Authorized access");
+      Serial.println();
+      rfid.PICC_HaltA();
+      rfid.PCD_StopCrypto1();
+      login = true;
+    }
+   
+    else   {
+      Serial.println("Access denied");
+      delay(3000);
+    }
   }
-
-  if ( ! rfid.PICC_ReadCardSerial()) {
-   return;
-  }
- MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
  
- Serial.print("Card: ");
- Serial.println(rfid.PICC_GetTypeName(piccType));
-
-  if(piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&
-  piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
-  piccType != MFRC522::PICC_TYPE_MIFARE_4K){
-    Serial.println("Máte špatná čip (Neni podporován"); 
-    return;
-  }
  
- for( byte i = 0; i < rfid.uid.size; i++){
-  Serial.print(rfid.uid.uidByte[i] < 0x10 ? "" : "0");
-  Serial.print(rfid.uid.uidByte[i], HEX);
+  delay(150);
+} 
 
- }
- 
 
- /* rfid.PICC_HaltA();
-  return 1;
-  */
 
   
- delay(1000);
-}
+ 
