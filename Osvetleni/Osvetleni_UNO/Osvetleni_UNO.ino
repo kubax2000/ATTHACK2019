97e096a9c2155318c0a584282a21b8e684fd30e8
Avik0 <mejlforbros@gmail.com>
2019-11-22 19:47:16 +0100

    Kompletní remaster s knihovnou, funkční :)

--- Osvetleni/Osvetleni_UNO/Osvetleni_UNO.ino
+++ Osvetleni/Osvetleni_UNO/Osvetleni_UNO.ino
@@ -1,3 +1,5 @@
+#include <FadeLed.h>
+
 const int s = 5;
 const int j = 6;
 const int v = 9;
@@ -9,28 +11,21 @@ const int pin_p_j = 3;
 const int pin_p_v = 4;
 const int pin_p_z = 7;
 
+FadeLed sever(s);
+FadeLed jih(j);
+FadeLed vychod(v);
+FadeLed zapad(z);
+FadeLed krizovatka(k);
+
 bool p_s;
 bool p_j;
 bool p_v;
 bool p_z;
 
-bool z_p_s;
-bool z_p_j;
-bool z_p_v;
-bool z_p_z;
+bool noc;
+int ft;
 
-int s_jas;
-int j_jas;
-int v_jas;
-int z_jas;
-int k_jas;
-
-bool s_sviti;
-bool j_sviti;
-bool v_sviti;
-bool z_sviti;
-
-void setup() {    
+void setup() {
     pinMode(s, OUTPUT);
     pinMode(j, OUTPUT);
     pinMode(v, OUTPUT);
@@ -41,67 +36,72 @@ void setup() {
     pinMode(pin_p_j, INPUT);
     pinMode(pin_p_v, INPUT);
     pinMode(pin_p_z, INPUT);
+
+    pinMode(A0, INPUT);
+
+    sever.setTime(5000, true);
+    jih.setTime(5000, true);
+    vychod.setTime(5000, true);
+    zapad.setTime(5000, true);
+    krizovatka.setTime(5500, true);
     
     Serial.begin(9600);
 }
 
 void loop() {
+    FadeLed::update();
+
     p_s = digitalRead(pin_p_s);
     Serial.println(p_s);
     p_j = digitalRead(pin_p_j);
     p_v = digitalRead(pin_p_v);
     p_z = digitalRead(pin_p_z);
+    ft = analogRead(A0);
+    
+    if(ft < 800) {
+      noc = 1;
+    }
+    else if(ft >= 800){
+      noc = 0;
+      sever.set(0);
+      jih.set(0);
+      vychod.set(0);
+      zapad.set(0);
+      krizovatka.set(0);
+    }
+    //Serial.println(noc);
 
-/*
-    if(p_s == 0 && s_sviti == 1){
-      s_jas--;
-      //Serial.println(s_jas);     
+    if(p_s == 1 && noc == 1) {
+      sever.set(255);
+      krizovatka.set(255);
     }
-    if(s_jas = 0) s_sviti = 0;
-*/    
-
-    if(p_s == 1 && s_sviti == 0){
-      s_jas++;
-      //Serial.println(s_jas);     
-    }
-    if(s_jas >= 180) {
-      s_sviti = 1;
-      s_jas = 255;
-    }
-
-    if(p_j == 1 && j_sviti == 0){
-      j_jas++;
-      //Serial.println(j_jas);     
-    }
-    if(j_jas >= 180) {
-      j_sviti = 1;
-      j_jas = 255;
+    else {
+      sever.set(0);
+      if(1) krizovatka.set(0);
     }
 
-    if(p_v == 1 && v_sviti == 0){
-      v_jas++;
-      //Serial.println(v_jas);     
+    if(p_j == 1 && noc == 1) {
+      jih.set(255);
+      krizovatka.set(255);
     }
-    if(v_jas >= 180) {
-      v_sviti = 1;
-      v_jas = 255;
+    else {
+      jih.set(0);
     }
 
-    if(p_z == 1 && z_sviti == 0){
-      z_jas++;
-      //Serial.println(z_jas);     
+    if(p_v == 1 && noc == 1) {
+      vychod.set(255);
+      krizovatka.set(255);
     }
-    if(z_jas >= 180) {
-      z_sviti = 1;
-      z_jas = 255;
+    else {
+      vychod.set(0);
+    }
+
+    if(p_z == 1 && noc == 1) {
+      zapad.set(255);
+      krizovatka.set(255);
+    }
+    else {
+      zapad.set(0);
     }
     
-    analogWrite(s, s_jas);
-    analogWrite(j, j_jas);
-    analogWrite(v, v_jas);
-    analogWrite(z, z_jas);
-    analogWrite(k, k_jas);
-
-    //Serial.println(s_jas);
-    delay(10);
 }
