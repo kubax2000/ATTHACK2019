d1f7dc532872de74db8d1fca65f32be0df94d1d5
Fligos <flig200@gmail.com>
2019-11-22 17:24:39 +0100

    README file path change; central program update

--- Central/Central_Program/sketch_nov22a/sketch_nov22a.ino
+++ Central/Central_Program/sketch_nov22a/sketch_nov22a.ino
@@ -14,6 +14,8 @@ pinMode (12, INPUT);   //sensor 2 (sv) pin 5
 pinMode (13, INPUT);   //sensor 2 (jv) pin 6
 pinMode (14, INPUT);   //sensor 2 (jz) pin 7
 pinMode (15, INPUT);   //sensor 2 (sz) pin 8
+pinMode (27, OUTPUT);  //semafor cervena
+pinMode (26, OUTPUT);  //
 
 
 
@@ -26,7 +28,7 @@ xTaskCreatePinnedToCore
 
 byte pin[8] = {32, 33, 34, 35, 12, 13, 14, 15};
 float rychlost[4];
-int aut[4] = {0, 0, 0, 0};
+int aut[4] = {0, 0, 0, 0);
 
 
 bool stav[8];
@@ -57,15 +59,12 @@ void Task1code( void * pvParameters )
       for(int i = 0; i < 4; i++) if(zmena(pin[i + 4])) rychlost[i] = 2 / (millis() - t[i]) * 1000; //vzdalennost senzoru = 2 m
 
 
-
-
-
-
-
       
      }
 }
 
+byte body_sj = 0;
+byte body_zv = 0;
   
 void loop() 
 {
