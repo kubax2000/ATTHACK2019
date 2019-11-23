b640a1162c12a3724c0edbdb78f70e975c643449
Fligos <flig200@gmail.com>
2019-11-23 02:40:00 +0100

    Rewrite to Arduino Genuino Uno

--- Central/Central_Program/MasterChip/MasterChip.ino
+++ Central/Central_Program/MasterChip/MasterChip.ino
@@ -1,6 +1,4 @@
 
-TaskHandle_t Task1;
-
 bool setup1[2][5] = {{1, 0, 0, 1, 0},{0, 1, 0, 1, 0}};
 bool setup2[2][5] = {{1, 0, 0, 1, 0},{1, 0, 0, 1, 0}};
 bool setup3[2][5] = {{1, 1, 0, 1, 0},{1, 0, 0, 1, 0}};
@@ -11,37 +9,38 @@ bool setup4[2][5] = {{0, 0, 1, 1, 0},{1, 0, 0, 0, 1}};
 
 void setup()
 {
+  
 Serial.begin(9600);  
-pinMode (32, INPUT);   //sensor 1 (sv) pin 0
-pinMode (33, INPUT);   //sensor 1 (jv) pin 1
-pinMode (34, INPUT);   //sensor 1 (jz) pin 2
-pinMode (35, INPUT);   //sensor 1 (sz) pin 3
-pinMode (12, INPUT);   //sensor 2 (sv) pin 4
-pinMode (13, INPUT);   //sensor 2 (jv) pin 5
-pinMode (14, INPUT);   //sensor 2 (jz) pin 6
-pinMode (15, INPUT);   //sensor 2 (sz) pin 7
-pinMode (27, OUTPUT);  //semafor cervena (sj)
-pinMode (26, OUTPUT);  //semafor zluta (sj)
-pinMode (25, OUTPUT);  //semafor zelena (sj)
-pinMode (2, OUTPUT);   //semafor chodci cervena (sj)
-pinMode (4, OUTPUT);   //semafor chodci zelena (sj)
-pinMode (18, OUTPUT);  //semafor cervena (vz)
-pinMode (19, OUTPUT);  //semafor zluta (vz)
-pinMode (21, OUTPUT);  //semafor zelena (vz)
-pinMode (22, OUTPUT);  //semafor chodci cervena (vz)
-pinMode (23, OUTPUT);  //semafor chodci zelena (vz)
+pinMode (2, INPUT);   //sensor 1 (sv) pin 0
+pinMode (3, INPUT);   //sensor 1 (jv) pin 1
+pinMode (4, INPUT);   //sensor 1 (jz) pin 2
+pinMode (5, INPUT);   //sensor 1 (sz) pin 3
+pinMode (6, INPUT);   //sensor 2 (sv) pin 4
+pinMode (7, INPUT);   //sensor 2 (jv) pin 5
+pinMode (8, INPUT);   //sensor 2 (jz) pin 6
+pinMode (9, INPUT);   //sensor 2 (sz) pin 7
+pinMode (10, OUTPUT);  //semafor cervena (sj)
+pinMode (11, OUTPUT);  //semafor zluta (sj)
+pinMode (12, OUTPUT);  //semafor zelena (sj)
+pinMode (13, OUTPUT);   //semafor chodci cervena (sj)
+pinMode (A0, OUTPUT);   //semafor chodci zelena (sj)
+pinMode (A1, OUTPUT);  //semafor cervena (vz)
+pinMode (A2, OUTPUT);  //semafor zluta (vz)
+pinMode (A3, OUTPUT);  //semafor zelena (vz)
+pinMode (A4, OUTPUT);  //semafor chodci cervena (vz)
+pinMode (A5, OUTPUT);  //semafor chodci zelena (vz)
 
 
-
-xTaskCreatePinnedToCore(Task1code, "Task1", 10000, NULL, 1, &Task1, 0);
 }
 
+
 int delta = 0;
-bool sempin[2][5] = {{27, 26, 25, 2, 4},{18, 19, 21, 22, 23}};
-byte pin[8] = {32, 33, 34, 35, 12, 13, 14, 15};
+bool sempin[2][5] = {{10, 11, 12, 13, A0},{A1, A2, A3, A4, A5}};
+byte pin[8] = {2, 3, 4, 5, 6, 7, 8, 9};
 float rychlost[4] = {0, 0, 0, 0};
 int aut[4] = {0, 0, 0, 0};
 byte cnt = 0;
+
 
 bool stav[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 bool zmena(int pinnum)
@@ -52,35 +51,6 @@ bool zmena(int pinnum)
     if(y != x) return 1;
     return 0;
   }
-
-
-
-
-void Task1code( void * pvParameters )
-{
-  long t[4];  //pamet casu
-
-  
-    while(true)
-     {      
-      for(int i = 0; i < 4; i++) if(zmena(i) && stav[i])
-      {
-      t[i] = millis();
-      aut[i]++;
-      }
-      for(int i = 0; i < 4; i++) 
-      {
-        if(zmena(i + 4) && stav[i + 4])
-        {
-        rychlost[i] = 2 / (float)(millis() - t[i]) * 1000;  //vzdalennost senzoru = 2 m
-        }
-      }
-      delay(50);
-     }
-}
-
-byte body_sj = 0;
-byte body_zv = 0;
 
 bool svetla[2][5];
 bool aktual;
@@ -100,12 +70,26 @@ bool aktual;
   }
   
 
-
+byte body_sj = 0;
+byte body_zv = 0;
   
 void loop() 
 {
 
+     long t[4];
+      
+      for(int i = 0; i < 4; i++) if(zmena(i) && stav[i])
+      {
+      t[i] = millis();
+      aut[i]++;
+      }
+      for(int i = 0; i < 4; i++) 
+      {
+        if(zmena(i + 4) && stav[i + 4])
+        {
+        rychlost[i] = 2 / (float)(millis() - t[i]) * 1000;  //vzdalennost senzoru = 2 m
+        }
+      } 
 
-
-  delay(50);
+  delay(10);
 }
