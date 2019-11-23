3f2ed77438c50dc159b3e7a025192f2fac5a1917
kubax2000 <jakub.marek42@gmail.com>
2019-11-23 04:35:55 +0100

    [] SSL update; GPS position get added;

--- web_app/app/forms/CrossroadForm/CrossroadForm.jsx
+++ web_app/app/forms/CrossroadForm/CrossroadForm.jsx
@@ -1,5 +1,6 @@
 import {Button, Checkbox, Form, Icon, Input, Label, List, Message, Select, Transition} from 'semantic-ui-react';
 import React, {useState} from 'react';
+import {PositionService} from '../../servicies/PositionService.jsx';
 import {RequestService} from '../../servicies/RequestService.jsx';
 
 export default function CrossroadForm() {
@@ -11,14 +12,16 @@ export default function CrossroadForm() {
         setState(newState);
     };
     const handleSubmit = () => {
+        let position = PositionService.getInfo();
+        if(undefined === position.speed) {
+            alert('Waiting for GPS');
+            return;
+        }
         RequestService.fetchUrl('');
     };
 
     return (
         <Form>
-            <Form.Field>
-                Are you on crossroad in 60 seconds?
-            </Form.Field>
             <Form.Field>
                 <label>Crossroad</label>
                 <Select name="crossroad_key" onChange={handleSelectChange} options={[{key: 'sv', value: 'sv', text: 'Severo východ'},{key: 'sz', value: 'sz', text: 'Severo západ'},{key: 'jv', value: 'jv', text: 'Jiho východ'},{ key: 'jz', value: 'jz', text: 'Jiho západ'}]} placeholder={'Select crossroad'} />
--- web_app/app/servicies/PositionService.jsx
+++ web_app/app/servicies/PositionService.jsx
@@ -1,12 +1,58 @@
 
+
 export const PositionService = {
+    getInfo,
     run
 };
 
-const run = () => {
-    setInterval(run_callback, 100);
+const positionServiceData = {
+    origin: {
+        latitude: undefined,
+        longitude: undefined,
+        speed: undefined,
+        timestamp: undefined
+    }
 };
 
-const run_callback = () => {
-    navigator.geolocation.getCurrentPosition(console.log)
-};
+function getInfo() {
+    return positionServiceData.origin;
+}
+
+function run() {
+    navigator.geolocation.watchPosition(position_success, position_error, {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+    });
+}
+
+function position_error(err) {
+    console.warn('ERROR(' + err.code + '): ' + err.message);
+}
+
+function position_success(position) {
+    console.log(position);
+    if(position.coords.latitude !== positionServiceData.origin.latitude || position.coords.longitude !== positionServiceData.origin.longitude) {
+        if(position.coords.accuracy > 1000) {
+            console.log('accuracy error');
+            return;
+        }
+        if(undefined !== positionServiceData.origin.timestamp) {
+            let lat1 = positionServiceData.origin.latitude / (180 / Math.PI);
+            let lat2 = position.coords.latitude / (180 / Math.PI);
+            let lon1 = positionServiceData.origin.longitude / (180 / Math.PI);
+            let lon2 = position.coords.longitude / (180 / Math.PI);
+
+            let dlon = lon2 - lon1;
+            let dlat = lat2 - lat1;
+            let a = Math.pow(Math.sin(dlat / 2), 2) + Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(dlon / 2), 2);
+
+            let c = 2 * Math.asin(Math.sqrt(a));
+
+            positionServiceData.origin.speed = (c * 6371 * 1.609344 * 1000000) / (position.timestamp - positionServiceData.origin.timestamp);
+        }
+        positionServiceData.origin.latitude = position.coords.latitude;
+        positionServiceData.origin.longitude = position.coords.longitude;
+        positionServiceData.origin.timestamp = position.timestamp;
+    }
+}
