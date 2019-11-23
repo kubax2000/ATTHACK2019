43d3a46432122ccaed34c89a7d77fe951ed7e094
Avik0 <mejlforbros@gmail.com>
2019-11-23 06:13:24 +0100

    První propojení

--- Osvetleni/Osvetleni_UNO/Osvetleni_UNO.ino
+++ Osvetleni/Osvetleni_UNO/Osvetleni_UNO.ino
@@ -1,33 +1,45 @@
+#include <Wire.h>
 #include <FadeLed.h>
 
-const int s = 5;
-const int j = 6;
-const int v = 9;
-const int z = 10;
-const int k = 11;
+//pins for lamp output
+const int s = 5; //north
+const int j = 6; //south
+const int v = 9; //east
+const int z = 10; //west
+const int k = 11; //crossroad center
 
+//PIR input pins
 const int pin_p_s = 2;
 const int pin_p_j = 3;
 const int pin_p_v = 4;
 const int pin_p_z = 7;
 
+//Defining pins for led fading
 FadeLed sever(s);
 FadeLed jih(j);
 FadeLed vychod(v);
 FadeLed zapad(z);
 FadeLed krizovatka(k);
 
+//PIR on/off
 bool p_s;
 bool p_j;
 bool p_v;
 bool p_z;
 
-bool noc;
-int ft;
-bool car;
-bool p_car;
+int ft; //Night sensor reading
+bool noc; //Is it night?
+bool car; //Is there a car on the crossroad
+bool p_car; //Previous state of car
+byte I2C_recieve;
+
 
 void setup() {
+    //Slave no.4 ready to recieve
+    Wire.begin(0x04);
+    Wire.setClock(1000);
+    Wire.onReceive(prichoziZnakI2C);
+    
     pinMode(s, OUTPUT);
     pinMode(j, OUTPUT);
     pinMode(v, OUTPUT);
@@ -38,11 +50,11 @@ void setup() {
     pinMode(pin_p_j, INPUT);
     pinMode(pin_p_v, INPUT);
     pinMode(pin_p_z, INPUT);
-
-    pinMode(A1, INPUT);
-
+    
+    //Night sensor
     pinMode(A0, INPUT);
 
+    //Time for the fade effect to take place for each section of LEDs
     sever.setTime(5000, true);
     jih.setTime(5000, true);
     vychod.setTime(5000, true);
@@ -52,9 +64,16 @@ void setup() {
     Serial.begin(9600);
 }
 
-void loop() {
-    FadeLed::update();
+//Function for recieving data
+void prichoziZnakI2C() {
+ I2C_recieve = Wire.read();
+}
 
+void loop() {
+    FadeLed::update(); //Needed for the fade effect to work properly
+    Serial.println(I2C_recieve);
+    if(I2C_recieve <= 1) car = I2C_recieve;
+    
     p_s = digitalRead(pin_p_s);
     //Serial.println(p_s);
     p_j = digitalRead(pin_p_j);
@@ -62,14 +81,10 @@ void loop() {
     p_z = digitalRead(pin_p_z);
     ft = analogRead(A0);
 
-    if(analogRead(A1) > 10) car = 1;
-    else car = 0;
-    Serial.println(car);
-
-    if(ft < 800) {
+    if(ft < 800) { //It's night and everything runs
       noc = 1;
     }
-    else if(ft >= 800){
+    else if(ft >= 800){ //Daytime-turn off all the lights just in case
       noc = 0;
       sever.set(0);
       jih.set(0);
@@ -77,19 +92,19 @@ void loop() {
       zapad.set(0);
       krizovatka.set(0);
     }
-    //Serial.println(noc);
+    //Serial.println(car);
 
     
-    
+    //Car approaching, turning the center lights on
     if(car == 1 && noc == 1) {
       krizovatka.set(255);
     }
     else if(p_car == 1 && car == 0){
       krizovatka.set(0);
     }
-
     p_car = car;
 
+    //North and center street light activated
     if(p_s == 1 && noc == 1) {
       sever.set(255);
       krizovatka.set(255);
@@ -98,6 +113,7 @@ void loop() {
       sever.set(0);
     }
 
+    //South and center street light activated
     if(p_j == 1 && noc == 1) {
       jih.set(255);
       krizovatka.set(255);
@@ -106,6 +122,7 @@ void loop() {
       jih.set(0);
     }
 
+    //East and center street light activated
     if(p_v == 1 && noc == 1) {
       vychod.set(255);
       krizovatka.set(255);
@@ -114,6 +131,7 @@ void loop() {
       vychod.set(0);
     }
 
+    //West and center street light activated
     if(p_z == 1 && noc == 1) {
       zapad.set(255);
       krizovatka.set(255);
@@ -121,7 +139,7 @@ void loop() {
     else {
       zapad.set(0);
     }
-    
+
     if(car == 0 && p_s == 0 && p_j == 0 && p_v == 0 && p_z == 0) krizovatka.set(0);
     
 }
